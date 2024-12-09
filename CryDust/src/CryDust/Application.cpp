33dc4ac4b07a7239b886b33c81e713bbd142db51
Maric <1109361945@qq.com>
2024-12-09 16:56:41 +0800

    添加Buffer Layout渲染缓冲布局

--- CryDust/src/CryDust/Application.cpp
+++ CryDust/src/CryDust/Application.cpp
@@ -22,7 +22,26 @@ namespace CryDust {
 
 	Application* Application::s_Instance = nullptr;
 
-	
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case CryDust::ShaderDataType::Float:    return GL_FLOAT;
+			case CryDust::ShaderDataType::Float2:   return GL_FLOAT;
+			case CryDust::ShaderDataType::Float3:   return GL_FLOAT;
+			case CryDust::ShaderDataType::Float4:   return GL_FLOAT;
+			case CryDust::ShaderDataType::Mat3:     return GL_FLOAT;
+			case CryDust::ShaderDataType::Mat4:     return GL_FLOAT;
+			case CryDust::ShaderDataType::Int:      return GL_INT;
+			case CryDust::ShaderDataType::Int2:     return GL_INT;
+			case CryDust::ShaderDataType::Int3:     return GL_INT;
+			case CryDust::ShaderDataType::Int4:     return GL_INT;
+			case CryDust::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		CORE_DEBUG_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
 
 	Application::Application()
 	{
@@ -41,15 +60,41 @@ namespace CryDust {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+					-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+					 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+					 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
+
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		//获得所有缓冲元素
+		for (const auto& element : layout)
+		{
+			//设置VAO
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -59,10 +104,13 @@ namespace CryDust {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			out vec3 v_Position;
+			out vec4 v_Color;
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -71,9 +119,11 @@ namespace CryDust {
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			in vec4 v_Color;
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
