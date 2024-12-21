55a90eb85b1ffa28db5761cb48626292de226ae5
Maric <1109361945@qq.com>
2024-12-21 15:33:59 +0800

    新的编辑器主题（橙黑色）

--- CryDust/src/CryDust/ImGui/ImGuiLayer.cpp
+++ CryDust/src/CryDust/ImGui/ImGuiLayer.cpp
@@ -49,6 +49,9 @@ namespace CryDust {
 			style.WindowRounding = 0.0f;
 			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
 		}
+
+		SetDarkThemeColors();
+
 		Application& app = Application::Get();
 		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
 
@@ -110,4 +113,32 @@ namespace CryDust {
 
 
 
+	void ImGuiLayer::SetDarkThemeColors()
+	{
+		auto& colors = ImGui::GetStyle().Colors;
+		colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+		// Headers
+		colors[ImGuiCol_Header] = ImVec4{ 0.9f, 0.605f, 0.21f, 1.0f };
+		colors[ImGuiCol_HeaderHovered] = ImVec4{ 1.0f, 0.705f, 0.31f, 1.0f };
+		colors[ImGuiCol_HeaderActive] = ImVec4{ 0.85f, 0.505f, 0.151f, 1.0f };
+
+		// Buttons
+		colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		// Frame BG
+		colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		// Tabs
+		colors[ImGuiCol_Tab] = ImVec4{ 0.85f, 0.6505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabHovered] = ImVec4{ 1.0f, 0.905f, 0.381f, 1.0f };
+		colors[ImGuiCol_TabActive] = ImVec4{ 1.0f, 0.825f, 0.281f, 1.0f };
+		colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.8f, 0.5505f, 0.01f, 1.0f };
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.86f, 0.605f, 0.01f, 1.0f };
+		// Title
+		colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+	}
 }
