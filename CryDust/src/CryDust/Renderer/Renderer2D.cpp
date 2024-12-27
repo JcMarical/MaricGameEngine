2239becb50c1187b11e20badfb98b8da4f8033d5
Maric <1109361945@qq.com>
2024-12-27 17:46:26 +0800

    修复TexIndex在纹理中插值的问题

--- CryDust/src/CryDust/Renderer/Renderer2D.cpp
+++ CryDust/src/CryDust/Renderer/Renderer2D.cpp
@@ -133,9 +133,8 @@ namespace CryDust {
 	void Renderer2D::BeginScene(const OrthographicCamera& camera)
 	{
 		CD_PROFILE_FUNCTION();
-
-		s_Data.TextureShader->Bind();
-		s_Data.TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
+		s_Data.CameraBuffer.ViewProjection = camera.GetViewProjectionMatrix();
+		s_Data.CameraUniformBuffer->SetData(&s_Data.CameraBuffer, sizeof(Renderer2DData::CameraData));
 
 		StartBatch();
 	}
@@ -344,7 +343,10 @@ namespace CryDust {
 
 	void Renderer2D::DrawSprite(const glm::mat4& transform, SpriteRendererComponent& src, int entityID)
 	{
-		DrawQuad(transform, src.Color, entityID);
+		if (src.Texture)
+			DrawQuad(transform, src.Texture, src.TilingFactor, src.Color, entityID);
+		else
+			DrawQuad(transform, src.Color, entityID);
 	}
 	/// <summary>
 	/// 重置统计数据
