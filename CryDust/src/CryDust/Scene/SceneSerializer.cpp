d66192a70ee29efdcc598c93fe73518ed3c448c8
Maric <1109361945@qq.com>
2025-04-13 22:14:22 +0800

    YAML解析错误提示

--- CryDust/src/CryDust/Scene/SceneSerializer.cpp
+++ CryDust/src/CryDust/Scene/SceneSerializer.cpp
@@ -245,6 +245,7 @@ namespace CryDust {
 		}
 		catch (YAML::ParserException e)
 		{
+			CORE_DEBUG_ERROR("Failed to load .hazel file '{0}'\n     {1}", filepath, e.what());
 			return false;
 		}
 		if (!data["Scene"])
