32a18a2ac151ab920b498c3448282314315c86dd
Maric <1109361945@qq.com>
2025-04-16 23:57:22 +0800

    SandBox可以在盒子外工作了

--- CryDust/src/CryDust/Core/Application.cpp
+++ CryDust/src/CryDust/Core/Application.cpp
@@ -23,8 +23,8 @@ namespace CryDust {
 
 	Application* Application::s_Instance = nullptr;
 
-	Application::Application(const std::string& name, ApplicationCommandLineArgs args)
-		: m_CommandLineArgs(args)
+	Application::Application(const ApplicationSpecification& specification)
+		: m_Specification(specification)
 	{
 
 		CD_PROFILE_FUNCTION();
@@ -33,7 +33,12 @@ namespace CryDust {
 		s_Instance = this;
 
 
-		m_Window = Window::Create(WindowProps(name));
+		// Set working directory here
+		if (!m_Specification.WorkingDirectory.empty())
+			std::filesystem::current_path(m_Specification.WorkingDirectory);
+
+		m_Window = Window::Create(WindowProps(m_Specification.Name));
+
 		m_Window->SetEventCallback(CD_BIND_EVENT_FN(Application::OnEvent));
 
 
